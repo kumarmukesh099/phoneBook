a366d1204e1d96767df72ae7662db7764cff476f
mukesh kumar <mk380345@gmail.com>
2020-10-20 23:50:44 +0530

    conatct home and installation

---
+++ client/src/component/layout/Pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="text-center">
+            <h1>About This App</h1>
+            <p >
+                This is a fullstack application for keeping contacts
+            </p>
+        </div>
+    )
+}
+
+export default About
