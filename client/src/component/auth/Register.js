672e1df6331830a3f9a1178e95819c850d6bb7fd
mukesh kumar <mk380345@gmail.com>
2021-04-01 00:33:52 +0530

    Minor Fix

--- client/src/component/auth/Register.js
+++ client/src/component/auth/Register.js
@@ -3,7 +3,7 @@ import alertContext from '../../context/alert/alertContext';
 //useState to add the component level state 
 const Register = () => {
     const AlertContext = useContext(alertContext);
-    const { setAlert} = AlertContext;
+    const { setAlert } = AlertContext;
     const [user, setUser] = useState({
         name: '',
         email: '',
@@ -17,10 +17,10 @@ const Register = () => {
 
     const onSubmit = (e) => {
         e.preventDefault();
-        if(name == '' || email == '' || password == '' || confirm_password == ''){
+        if (name == '' || email == '' || password == '' || confirm_password == '') {
             setAlert('Please enter all the fields', 'danger');
         }
-        else if(password != confirm_password){
+        else if (password != confirm_password) {
             setAlert('Password and Confirm Password should be same', 'danger');
         }
         else {
@@ -45,15 +45,15 @@ const Register = () => {
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" value={password} onChange={onChange} 
-                    required minLength="6"/>
+                    <input type="password" name="password" value={password} onChange={onChange}
+                        required minLength="6" />
                 </div>
                 <div className="form-group">
                     <label htmlFor="confirm_password">Confirm Password</label>
-                    <input type="password" name="confirm_password" value={confirm_password} onChange={onChange} 
-                    required minLength='6'/>
+                    <input type="password" name="confirm_password" value={confirm_password} onChange={onChange}
+                        required minLength='6' />
                 </div>
-                <input className="btn btn-primary btn-block" type="submit" value="Register"/>
+                <input className="btn btn-primary btn-block" type="submit" value="Register" />
             </form>
         </div>
     )
